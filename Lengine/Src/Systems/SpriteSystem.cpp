431144e41a23db68d404637451ecf274636cbcfe
LioQing <s2010157@gmail.com>
2020-05-15 10:55:51 +0800

    Add draw order to entities

--- Lengine/Src/Systems/SpriteSystem.cpp
+++ Lengine/Src/Systems/SpriteSystem.cpp
@@ -18,42 +18,18 @@ void SpriteSystem::Update(lecs::EntityManager* entity_manager, lecs::EventManage
 
 void SpriteSystem::LateUpdate(lecs::EntityManager* entity_manager, lecs::EventManager* event_manager, DeltaTime dt)
 {
+	std::vector<std::future<void>> results;
 	for (auto& e : entity_manager->EntityFilter<TransformComponent>().EntityFilter<SpriteComponent>().entities)
 	{
-		TransformComponent* transform = &e->GetComponent<TransformComponent>();
-		SpriteComponent* sprite = &e->GetComponent<SpriteComponent>();
-
-		// scale
-		sprite->sprite.setScale(transform->scale.sfVector2f());
-
-		// animation
-		if (e->HasComponent<AnimationComponent>())
-		{
-			AnimationComponent* animation = &e->GetComponent<AnimationComponent>();
-			sf::IntRect srcRect;
-			srcRect.width = transform->width;
-			srcRect.height = transform->height;
-			if (animation->current.delay != 0)
-			{
-				srcRect.left = srcRect.width * (static_cast<int>(animation->timer / animation->current.delay) % animation->current.frame);
-			}
-			else
-			{
-				game->logger->AddLog
-				(
-					"Error: Animation Component current animation delay is 0, cannot divide by 0",
-					lecs::LT_ERROR, lecs::LT_COMPONENT
-				);
-			}
-			srcRect.top = animation->current.index * transform->height;
-
-			sprite->sprite.setTextureRect(srcRect);
-		}
+		results.emplace_back(game->tp.enqueue(&SpriteSystem::SubLateUpdate, this, entity_manager, event_manager, dt, e));
 	}
+	for (auto& r : results) r.get();
 }
 
 void SpriteSystem::Draw(lecs::EntityManager* entity_manager, lecs::EventManager* event_manager, sf::RenderWindow* window)
 {
+	std::vector<sf::Sprite*> draw_order;
+
 	for (auto& e : entity_manager->EntityFilter<TransformComponent>().EntityFilter<SpriteComponent>().entities)
 	{
 		TransformComponent* transform = &e->GetComponent<TransformComponent>();
@@ -70,7 +46,55 @@ void SpriteSystem::Draw(lecs::EntityManager* entity_manager, lecs::EventManager*
 			continue;
 		}
 
-		// draw
-		window->draw(sprite->sprite);
+		// insert
+		draw_order.push_back(&sprite->sprite);
+	}
+
+	for (int x = 0; x < draw_order.size(); x++)
+	{
+		for (int y = 0; y < draw_order.size() - 1; y++)
+		{
+			if (draw_order[y]->getGlobalBounds().top + draw_order[y]->getGlobalBounds().height > draw_order[y + 1]->getGlobalBounds().top + draw_order[y + 1]->getGlobalBounds().height)
+			{
+				sf::Sprite* temp = draw_order[y + 1];
+				draw_order[y + 1] = draw_order[y];
+				draw_order[y] = temp;
+			}
+		}
+	}
+
+	for (auto s : draw_order) window->draw(*s);
+}
+
+void SpriteSystem::SubLateUpdate(lecs::EntityManager* entity_manager, lecs::EventManager* event_manager, DeltaTime dt, lecs::Entity* e)
+{
+	TransformComponent* transform = &e->GetComponent<TransformComponent>();
+	SpriteComponent* sprite = &e->GetComponent<SpriteComponent>();
+
+	// scale
+	sprite->sprite.setScale(transform->scale.sfVector2f());
+
+	// animation
+	if (e->HasComponent<AnimationComponent>())
+	{
+		AnimationComponent* animation = &e->GetComponent<AnimationComponent>();
+		sf::IntRect srcRect;
+		srcRect.width = transform->width;
+		srcRect.height = transform->height;
+		if (animation->current.delay != 0)
+		{
+			srcRect.left = srcRect.width * (static_cast<int>(animation->timer / animation->current.delay) % animation->current.frame);
+		}
+		else
+		{
+			game->logger->AddLog
+			(
+				"Error: Animation Component current animation delay is 0, cannot divide by 0",
+				lecs::LT_ERROR, lecs::LT_COMPONENT
+			);
+		}
+		srcRect.top = animation->current.index * transform->height;
+
+		sprite->sprite.setTextureRect(srcRect);
 	}
 }
